c06f9175dcc7b54766a1446ab7ab49b2c791da54
Josue Daniel Guerrero Ballestero <iinoted@gmail.com>
2019-03-15 18:08:15 +0100

    Add vuetify, fix layout

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -1,7 +1,104 @@
 <template>
-  <div>
+  <!-- <div>
     <h2>Dashboard</h2>
     <p>Name: {{ user.name }}</p>
+  </div> -->
+  <div>
+    <v-navigation-drawer
+      v-model="drawer"
+      :clipped="$vuetify.breakpoint.lgAndUp"
+      fixed
+      app
+    >
+      <v-list dense>
+        <template v-for="item in items">
+          <v-layout v-if="item.heading" :key="item.heading" row align-center>
+            <v-flex xs6>
+              <v-subheader v-if="item.heading">
+                {{ item.heading }}
+              </v-subheader>
+            </v-flex>
+            <v-flex xs6 class="text-xs-center">
+              <a href="#!" class="body-2 black--text">EDIT</a>
+            </v-flex>
+          </v-layout>
+          <v-list-group
+            v-else-if="item.children"
+            :key="item.text"
+            v-model="item.model"
+            :prepend-icon="item.model ? item.icon : item['icon-alt']"
+            append-icon=""
+          >
+            <template v-slot:activator>
+              <v-list-tile>
+                <v-list-tile-content>
+                  <v-list-tile-title>
+                    {{ item.text }}
+                  </v-list-tile-title>
+                </v-list-tile-content>
+              </v-list-tile>
+            </template>
+            <v-list-tile v-for="(child, i) in item.children" :key="i" @click="">
+              <v-list-tile-action v-if="child.icon">
+                <v-icon>{{ child.icon }}</v-icon>
+              </v-list-tile-action>
+              <v-list-tile-content>
+                <v-list-tile-title>
+                  {{ child.text }}
+                </v-list-tile-title>
+              </v-list-tile-content>
+            </v-list-tile>
+          </v-list-group>
+          <v-list-tile v-else :key="item.text" @click="">
+            <v-list-tile-action>
+              <v-icon>{{ item.icon }}</v-icon>
+            </v-list-tile-action>
+            <v-list-tile-content>
+              <v-list-tile-title>
+                {{ item.text }}
+              </v-list-tile-title>
+            </v-list-tile-content>
+          </v-list-tile>
+        </template>
+      </v-list>
+    </v-navigation-drawer>
+    <v-toolbar
+      :clipped-left="$vuetify.breakpoint.lgAndUp"
+      color="#27293d"
+      dark
+      app
+      fixed
+    >
+      <v-toolbar-title style="width: 300px" class="ml-0 pl-3">
+        <v-toolbar-side-icon
+          @click.stop="drawer = !drawer"
+        ></v-toolbar-side-icon>
+        <span class="hidden-sm-and-down">Dashboard</span>
+      </v-toolbar-title>
+      <v-text-field
+        flat
+        solo-inverted
+        hide-details
+        prepend-inner-icon="search"
+        label="Search"
+        class="hidden-sm-and-down"
+      ></v-text-field>
+      <v-spacer></v-spacer>
+    </v-toolbar>
+    <v-content>
+      <v-container fluid fill-height>
+        <v-layout justify-center align-center>
+          <v-tooltip right>
+            <template v-slot:activator="{ on }">
+              <v-btn icon large v-on="on">
+                <v-icon large>code</v-icon>
+              </v-btn>
+            </template>
+            <span>Source</span>
+          </v-tooltip>
+        </v-layout>
+      </v-container>
+    </v-content>
   </div>
 </template>
 
@@ -14,7 +111,38 @@ export default {
   data: () => ({
     user: {
       name: "Jesse"
-    }
+    },
+    drawer: false,
+    items: [
+      { icon: "contacts", text: "Contacts" },
+      { icon: "history", text: "Frequently contacted" },
+      { icon: "content_copy", text: "Duplicates" },
+      {
+        icon: "keyboard_arrow_up",
+        "icon-alt": "keyboard_arrow_down",
+        text: "Labels",
+        model: true,
+        children: [{ icon: "add", text: "Create label" }]
+      },
+      {
+        icon: "keyboard_arrow_up",
+        "icon-alt": "keyboard_arrow_down",
+        text: "More",
+        model: false,
+        children: [
+          { text: "Import" },
+          { text: "Export" },
+          { text: "Print" },
+          { text: "Undo changes" },
+          { text: "Other contacts" }
+        ]
+      },
+      { icon: "settings", text: "Settings" },
+      { icon: "chat_bubble", text: "Send feedback" },
+      { icon: "help", text: "Help" },
+      { icon: "phonelink", text: "App downloads" },
+      { icon: "keyboard", text: "Go to the old version" }
+    ]
   }),
   mounted() {
     this.getUserData();
--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,14 +1,38 @@
 <template>
-  <div>
-    <h2>Login</h2>
-    <form @submit.prevent="login">
-      <input type="text" name="email" />
-      <br />
-      <input type="password" name="password" />
-      <br />
-      <input type="submit" value="Login" />
-    </form>
-  </div>
+  <v-container fluid fill-height>
+    <v-layout align-center justify-center>
+      <v-flex xs12 sm8 md4>
+        <v-card class="elevation-12">
+          <v-toolbar dark color="#27293d">
+            <v-toolbar-title>Login</v-toolbar-title>
+          </v-toolbar>
+          <v-card-text>
+            <v-form>
+              <v-text-field
+                v-model="email"
+                prepend-icon="person"
+                name="email"
+                label="Email"
+                type="text"
+              ></v-text-field>
+              <v-text-field
+                id="password"
+                v-model="password"
+                prepend-icon="lock"
+                name="password"
+                label="Password"
+                type="password"
+              ></v-text-field>
+            </v-form>
+          </v-card-text>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn dark color="#344675" @click="login">Login</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
@@ -16,15 +40,18 @@ import axios from "axios";
 import router from "@/router";
 export default {
   name: "Login",
+  data: () => ({
+    email: "user@email.com",
+    password: "password"
+  }),
   methods: {
-    login: () => {
-      let email = "user@email.com";
-      let password = "password";
+    login() {
       let login = () => {
         let data = {
-          email: email,
-          password: password
+          email: this.email,
+          password: this.password
         };
+        console.log(data);
 
         axios
           .post("/api/login", data)
